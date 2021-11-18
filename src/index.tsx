4e32cdbdb9b3d656658c6cf2c9f4508c8250454f
qiaozhen03 <qiaozhen03@58.com>
2021-11-18 10:30:51 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Button } from 'antd';
 const transFormParam = (filterJson)=>{
     
 }
 const CreateFilter =(param)=>{
-    console.log(param)
+    console.log(param);
+    return (
+        <div>
+            <Button/>
+        </div>
+    )
 } 
+export {CreateFilter}
