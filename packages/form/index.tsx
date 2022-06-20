e2b4622503405a64adab782165fda4afb2957b2a
qiaozhen <1197433123@qq.com>
2022-06-20 14:46:54 +0800

    config

--- packages/form/index.tsx
+++ packages/form/index.tsx
@@ -1,24 +1,29 @@
-import React,{ComponentProps} from "react";
-import {Form as OriginalForm, FormInstance} from "antd";
+import React, { ComponentProps } from 'react'
+import { Form as OriginalForm, FormInstance } from 'antd'
 
 type FormProps = ComponentProps<typeof OriginalForm>
 type FormInterface = typeof OriginalForm
-export const FormCmp:React.ForwardRefRenderFunction<any, FormProps> = ({children, ...props}:FormProps, ref)=>{
-  return <OriginalForm
-    ref={ref}
-    {...props}
-  >
-    {children}
-  </OriginalForm>
+export const FormCmp: React.ForwardRefRenderFunction<any, FormProps> = (
+  { children, ...props }: FormProps,
+  ref
+) => {
+  return (
+    <OriginalForm ref={ref} {...props}>
+      {children}
+    </OriginalForm>
+  )
 }
-const FormForwardRef = React.forwardRef<any, FormProps>(FormCmp) as <Values = any>(props: FormInterface & {
-  children?: React.ReactNode;
-} & {
-  ref?: React.Ref<FormInstance<Values>> | undefined;
-}) => React.ReactElement
-let Form: FormInterface;
-Form = FormForwardRef as FormInterface;
-
+const FormForwardRef = React.forwardRef<any, FormProps>(FormCmp) as <
+  Values = any
+>(
+  props: FormInterface & {
+    children?: React.ReactNode
+  } & {
+    ref?: React.Ref<FormInstance<Values>> | undefined
+  }
+) => React.ReactElement
+let Form
+Form = FormForwardRef as FormInterface
 
 Form.useForm = OriginalForm.useForm
 Form.Item = OriginalForm.Item
@@ -26,6 +31,12 @@ Form.List = OriginalForm.List
 Form.ErrorList = OriginalForm.ErrorList
 Form.Provider = OriginalForm.Provider
 
-export {Form}
+var a = {
+  a: '123',
+  b: {},
+  c: '123',
 
-
+  d: { v: '456', o: '999' }
+}
+console.log('123')
+export { Form }
